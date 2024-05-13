b713ec60b654e4cb713e9aa6640785fa3351d639
Arkan Mahardika <arkan.mahardika.1@gmail.com>
2024-05-13 20:40:55 +0700

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,126 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.ktlint)
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "com.arkan.seeflix"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.arkan.seeflix"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+
+        buildConfig = true
+    }
+    flavorDimensions += "env"
+    productFlavors {
+        create("production") {
+            buildConfigField(
+                type = "String",
+                name = "BASE_URL",
+                value = "\"https://api.themoviedb.org/3/\"",
+            )
+            buildConfigField(
+                type = "String",
+                name = "ACCESS_TOKEN",
+                value = "\"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOGNmMmFiMDM0NThlZmE0ZmIxNjE4ODZjM2Y3Y2RjYiIsInN1YiI6IjY2NDIwOGE3NDBhNDM3MDQ0OTU1YzM0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mVp6AFddcF_CSHj6lWNZYlIwgR3YfgQycHKJ8XKCTS0\"",
+            )
+        }
+        create("integration") {
+            buildConfigField(
+                type = "String",
+                name = "BASE_URL",
+                value = "\"https://api.themoviedb.org/3/\"",
+            )
+            buildConfigField(
+                type = "String",
+                name = "ACCESS_TOKEN",
+                value = "\"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOGNmMmFiMDM0NThlZmE0ZmIxNjE4ODZjM2Y3Y2RjYiIsInN1YiI6IjY2NDIwOGE3NDBhNDM3MDQ0OTU1YzM0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mVp6AFddcF_CSHj6lWNZYlIwgR3YfgQycHKJ8XKCTS0\"",
+            )
+        }
+    }
+}
+
+tasks.getByPath("preBuild").dependsOn("ktlintFormat")
+
+ktlint {
+    android.set(false)
+    ignoreFailures.set(true)
+    reporters {
+        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
+        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
+    }
+    kotlinScriptAdditionalPaths {
+        include(fileTree("scripts/"))
+    }
+    filter {
+        exclude("**/generated/**")
+        include("**/kotlin/**")
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.coil)
+    implementation(libs.lifecycle.livedata)
+    implementation(libs.lifecycle.runtime)
+    implementation(libs.lifecycle.viewmodel)
+    implementation(libs.fragment.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+    implementation(libs.coroutine.core)
+    implementation(libs.coroutine.android)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+    implementation(libs.okhttp)
+    implementation(libs.koin.android)
+    implementation("com.squareup.okhttp3:logging-interceptor:4.5.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("com.airbnb.android:lottie:3.4.0")
+    testImplementation(libs.junit)
+    testImplementation(libs.mockk.agent)
+    testImplementation(libs.coroutine.test)
+    testImplementation(libs.turbine)
+    testImplementation(libs.core.testing)
+    androidTestImplementation(libs.mockk.android)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
